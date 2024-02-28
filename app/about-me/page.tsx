a9e1a784607ef00dfd6af2e658645c87f89dcf0d
NekoJar <mzharfanhakim7@gmail.com>
2024-02-28 23:58:07 +0700

    updating styles

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const AboutMePage = () => {
-  return <div>AboutMePage</div>;
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <div>
+      <h1 className="font-extrabold">About Me</h1>
+      <button onClick={() => setOpen(!isOpen)}>See me!!</button>
+      {isOpen && <p>aoskdo</p>}
+
+      <Link href="/" className="hover:text-red-400">
+        Go home!
+      </Link>
+    </div>
+  );
 };
 
 export default AboutMePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello Next!</div>;
+  return (
+    <div>
+      
+      <Link href="./about-me" className="hover:text-red-400">
+        About me
+      </Link>
+    </div>
+  );
 }
