5a57503667d1f859689d76fffa0287fcbf1210e9
NekoJar <mzharfanhakim7@gmail.com>
2024-03-07 23:39:49 +0700

    updating styles and preparing for next content

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const AboutMePage = () => {
   const [isOpen, setOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <div>
       <h1 className="font-extrabold">About Me</h1>
-      <button onClick={() => setOpen(!isOpen)}>See me!!</button>
-      {isOpen && <p>aoskdo</p>}
+      <div className="flex flex-col w-32 mt-4 ml-4 gap-2 border p-4 text-center">
+        <button className="hover:text-red-400" onClick={() => setOpen(!isOpen)}>
+          {isOpen ? "Close me!!" : "Click me!!"}
+        </button>
+        {isOpen && <p className="text-red-700">hellow</p>}
 
-      <Link href="/" className="hover:text-red-400">
-        Go home!
-      </Link>
+        <button
+          className="bg-black rounded-md border text-white p-2 hover:bg-slate-300 hover:text-black transition-colors"
+          type="button"
+          onClick={() => router.push("/")}
+        >
+          Back to home
+        </button>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div>
-      <Link href="./about-me" className=" hover:text-red-400">
-        About me
-      </Link>
+    <div className="flex flex-col w-32 mt-4 ml-4 gap-8 border p-4 text-center">
+      <div>
+        <p>Normal link: </p>
+        <Link href="./about-me" className=" hover:text-red-400">
+          About me
+        </Link>
+      </div>
 
-      <button type="button" onClick={() => router.push("/about-me")}>
-        About me
-      </button>
+      <div>
+        <p>Button: </p>
+        <button
+          className="bg-black rounded-md border text-white p-2 hover:bg-slate-300 hover:text-black transition-colors"
+          type="button"
+          onClick={() => router.push("/about-me")}
+        >
+          About me
+        </button>
+      </div>
     </div>
   );
 }
